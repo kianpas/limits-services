572c39a675d5bfdb6447a340657aa77a776fb704
kianpas <kianpas@gmail.com>
2023-12-24 16:37:08 +0900

    2023.12.24 limits-service 제한 yml 이동

---
+++ src/main/java/com/minutes/microservices/limitsservices/configuration/Configuration.java
@@ -0,0 +1,15 @@
+package com.minutes.microservices.limitsservices.configuration;
+
+import lombok.*;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Component
+@ConfigurationProperties("limits-service")
+@Data
+@AllArgsConstructor
+public class Configuration {
+
+    private int minimum;
+    private int maximum;
+}
--- src/main/java/com/minutes/microservices/limitsservices/controller/LimitsController.java
+++ src/main/java/com/minutes/microservices/limitsservices/controller/LimitsController.java
@@ -1,5 +1,6 @@
 package com.minutes.microservices.limitsservices.controller;
 
+import com.minutes.microservices.limitsservices.configuration.Configuration;
 import com.minutes.microservices.limitsservices.controller.bean.Limits;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,9 +8,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class LimitsController {
 
+    private Configuration configuration;
     @GetMapping("/limits")
     public Limits retrieveLimits() {
-        return new Limits(1, 1000);
+        return new Limits(configuration.getMinimum(), configuration.getMaximum());
 
     }
 }
